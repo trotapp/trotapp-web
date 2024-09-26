627ea1dbc4729a87ac8570af21898f7faa30364f
JuanZapata <jdzapatamar@cesde.net>
2024-09-26 14:59:17 -0500

    subida de rama

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './infrastructure/config/i18next';
+import Benefits from '../src/presentation/components/Benefits.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Benefits />
   </StrictMode>
 );
