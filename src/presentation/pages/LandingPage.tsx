91966eab39a9251a0c30df941bc566f74aa8b701
Camilo Andres Bolaños Andrade <camiloa.andresc@hotmail.com>
2024-09-30 14:40:32 -0500

    feat: Se agregaron dos secciones existentes a la LandingPage

--- src/presentation/pages/LandingPage.tsx
+++ src/presentation/pages/LandingPage.tsx
@@ -1,13 +1,11 @@
-import Mockup from "../components/mockup/Mockup";
+import Banner from '../components/Banner/Banner';
+import Mockup from '../components/mockup/Mockup';
 
 export const LandingPage = () => {
   return (
     <div>
-      <div className="pt-32"></div>
-      <div className="p-2">
+      <Banner />
       <Mockup />
-      </div>
-      LandingPage
     </div>
   );
 };
