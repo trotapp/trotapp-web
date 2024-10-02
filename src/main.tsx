424dc1e1b091f7614fdde49b966633fd10ed20ee
JuanZapata <jdzapatamar@cesde.net>
2024-10-02 16:08:46 -0500

    ultimo commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './infrastructure/config/i18next';
-import Benefits from '../src/presentation/components/Benefits.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Benefits />
-  </StrictMode>
+=  </StrictMode>
 );
