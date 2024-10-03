613b55613c017385476673bd7e06926e491fa919
JuanZapata <jdzapatamar@cesde.net>
2024-10-02 22:21:41 -0500

    Se crea nueva rama (Juan David Zapata) por daños en la anterior, se crea nuevamente el archivo y se siguen correcciones planteadas

--- src/presentation/pages/LandingPage.tsx
+++ src/presentation/pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import Banner from '../components/Banner/Banner';
 import Mockup from '../components/mockup/Mockup';
+import Benefits from '../components/Benefits/Benefits';
 
 export const LandingPage = () => {
   return (
     <div>
       <Banner />
       <Mockup />
+      <Benefits />
     </div>
   );
 };
